776a28f2fa0c15651b65ba5ecba60abe24da6c02
WilsonZanee <wilson.zane@gmail.com>
2024-09-05 17:02:00 -0500

    Finished work for HW1.  Probably should check some of this by hand

--- hw1.py
+++ hw1.py
@@ -1,5 +1,7 @@
 import datetime as t
+import os
 
+import numpy as np
 from poliastro.bodies import Earth, Sun
 from poliastro.twobody import Orbit
 from poliastro.constants.general import GM_earth, GM_sun, R_mean_earth
@@ -7,8 +9,13 @@ from astropy import units as u
 from astropy import constants as C
 
 import two_body_util as util
+from orbits import Orbit
 
-# Problem 2: Halley's Commet
+width = os.get_terminal_size().columns
+
+# Problem 2: Halley's Commet --------------------------------------------------
+print("\n" + "-"*width)
+print("Problem 2: Halley's Commet\n")
 
 period = util.elliptical_period(17.9564, 1)
 print(f"Orbital Period in canonical units: {period}")
@@ -20,16 +27,16 @@ orbit_end_time = orbit_start_time + time_delta
 print(f"Orbital Period in days: {period_days}")
 print(f"Halley's comet will return to perihelion: {orbit_end_time}")
 
-# Question 3: Earth Satellite
+
+# Question 3: Earth Satellite -------------------------------------------------
+print("\n" + "-"*width)
+print("Question 3: Earth Satellite\n")
 
 meu = 1*util.MEU_EARTH
-print(meu)
 p = 13778*(u.km)
 p = p.to(util.DU_EARTH)
-print(p)
 velo_p = 6.5*(u.km/u.s)
 velo_p = velo_p.to(util.DUTU_EARTH)
-print(velo_p)
 momentum = util.angular_momentum_from_p(p.value, meu.value)
 print(f"Momentum: {momentum}")
 spec_energy = util.specific_energy_from_velo(velo_p.value, meu.value, p.value)
@@ -38,6 +45,26 @@ a = util.semi_major_axis_from_energy(spec_energy, meu.value)
 print(f"a: {a}")
 e = util.eccentricity_from_momentum_energy(momentum, spec_energy, meu.value)
 print(f"Eccentricity: {e}")
+theta = (100*u.deg).to(u.rad)
+r = util.orbit_radius_from_p_eccentricity_true_anomaly(e, p, theta.value)
+print(f"Radius of orbit at true anomaly {theta} is {r}")
+
+
+# Question 4: Satellite OE ----------------------------------------------------
+print("\n" + "-"*width)
+print("Question 4: Satellite OE\n")
+
+r = np.array([6578, 6778, 6178])*u.km
+v = np.array([-4.5, 2.5, 5.5])*u.km/u.s
+meu = (1*util.MEU_EARTH).to(u.km**3/u.s**2)
+
+orbit = Orbit(r_vector=r, v_vector=v, meu=meu)
+print(f"Angular Momentum Vector: {orbit.angular_momentum}"
+      " = {orbit.angular_momentum.to(util.MOMENTUM_EARTH)}")
+print(f"Specific Energy: {orbit.spec_energy}")
+print(f"Eccentricity: {orbit.eccentricity}")
+print(f"rp: {orbit.rp} = {orbit.rp.to(util.DU_EARTH)}")
+print(f"ra: {orbit.ra} = {orbit.ra.to(util.DU_EARTH)}")
 
 
 
--- two_body_util.py
+++ two_body_util.py
@@ -8,17 +8,24 @@ DU_EARTH = u.def_unit("Du Earth", 6379.1*u.km)
 TU_EARTH = u.def_unit("Tu Earth", 806.8*u.s) 
 DUTU_EARTH = u.def_unit("Du/Tu Earth", 7.9053661*u.km/u.s) 
 MEU_EARTH = u.def_unit("meu Earth", 3.986e5*u.km**3/u.s**2)
+SPEC_E_EARTH = u.def_unit("Du^2/s^2", DU_EARTH**2/TU_EARTH**2)
+MOMENTUM_EARTH = u.def_unit("Du^2/s", DU_EARTH**2/TU_EARTH)
 
 AU_SUN = u.def_unit("Au Sun", 1.496e8*u.km) 
 TU_SUN = u.def_unit("Tu Sun", 58.132821*u.d) 
 AUTU_SUN = u.def_unit("Au/Tu Sun", 29.784852*u.km/u.s) 
 MEU_SUN = u.def_unit("meu Sun", 1.3271544e11*u.km**3/u.s**2)
 
+spec_energy = u.km**2/u.s**2
+angular_momentum = u.km**2/u.s
+
+
 
 def elliptical_period(a, meu):
     period = 2*pi*a**(3/2) / sqrt(meu)
     return period
 
+#************************* Conservative Variables *****************************
 def specific_energy_from_velo(velo, meu, radius):
     energy = (velo**2/2) - (meu/radius)
     return energy
@@ -27,6 +34,7 @@ def angular_momentum_from_p(p, meu):
     momentum = sqrt(p*meu) 
     return momentum
 
+#************************ Orbital Elements ************************************
 def semi_major_axis_from_energy(energy, meu):
     a = -meu/(2*energy)
     return a
@@ -35,6 +43,8 @@ def eccentricity_from_momentum_energy(angular_momentum, energy, meu):
     e = sqrt(1 + (2*angular_momentum**2*energy/meu**2))
     return e
 
+
+#************************ Radius and Velocity Vectors *************************
 def orbit_radius_from_p_eccentricity_true_anomaly(e, p, theta):
     r = p/(1+e*cos(theta))
     return r
