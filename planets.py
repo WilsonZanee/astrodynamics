7ceaf476277ba897bf3d5c2d046e5af120a48599
WilsonZanee <wilson.zane@gmail.com>
2024-11-05 14:55:31 -0600

    Started work on planets

---
+++ planets.py
@@ -0,0 +1,16 @@
+
+from datetime import datetime
+
+import numpy as np
+
+import MAE469_ProjectLibrary as PROJ
+
+class Planet:
+    def __init__(self, orbital_elements, epoch):
+        self.epoch_oe = orbital_elements
+        self.epoch = epoch
+
+    def get_eos_at_time(self, time):
+
+        oe = 
+        return oe
