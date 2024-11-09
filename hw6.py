Mostly completed problems 1-3.  Fixed an issue in the interplanetary_transfer_dv func that was giving incorrect results.  Completed some code for gravity assists.  I tried to check against an example but the example had errors in it.

hw6.py
@@ -62,7 +62,22 @@ print(f"The total dv is {dv}")
 print("\n" + "-"*width + "\n")
 print("Question 2: Jupiter Fly-by\n")
 
-e_hohmann = util.eccentricity_from_rarp(r_earth, r_jupiter)
+escape_velo = util.get_escape_velo(mu_sun, r_jupiter)
+transfer_energy = util.specific_energy_from_rpra(r_earth, r_jupiter, mu_sun)
+vt1 = util.velo_from_energy(transfer_energy, mu_sun, r_earth)
+vt2 = util.velo_from_energy(transfer_energy, mu_sun, r_jupiter)
+
+# Gravity Assist
+v_jupiter_mag = util.velo_from_radius(mu_sun, r_jupiter, r_jupiter)
+v_jupiter = np.array([[v_jupiter_mag.value], [0], [0]])*v_jupiter_mag.unit
+v_t2 = np.array([[vt2.value], [0], [0]])*vt2.unit
+rp = 71398*u.km
+
+v_final, vf_mag = util.get_gravity_assist_velo(rp, v_t2, v_jupiter, mu_jupiter, 
+                                               debug=False)
+print(f"To escape the Sun's influence, a S/C would need {escape_velo} of velo"
+      f" where as our S/C will only have {vf_mag} of velo after getting a" 
+      " gravity assist from jupiter.  Our S/C won't have enough velocity.")
 
 
 # Question 3: Earth-Mars Planetary Transfer -----------------------------------
@@ -80,31 +95,66 @@ flightpath_angle_lander = -12*u.deg
 r_final_orbiter = 400*u.km + 3389.5*u.km
 
 # 3a - Determine heliocentric r,v at Mars arrival
+print("Part 3a - Spacecraft radius and velo at mars entry: ")
 r_at_mars, v_at_mars = util.time_of_flight_universal_var(r_at_earth,
                                                          v_at_earth,
                                                          dt,
                                                          mu_sun)
-print("Part 3a - Spacecraft radius and velo at mars entry: "
-      f"\n {r_at_mars} = {r_at_mars.to(u.km)}"
+
+print(f"\n {r_at_mars} = {r_at_mars.to(u.km)}"
       f"\n{v_at_mars.to(util.AUTU_SUN)} = {v_at_mars.to(u.km/u.s)}")
 
 # 3b - Determine oe of transfer orbit
+print("\nPart 3b - Transfer orbit orbital elements:")
 transfer_orbit = Orbit(r_vector=r_at_earth, v_vector=v_at_earth, meu=mu_sun)
-print("Part 3b - Transfer orbit orbital elements:")
 print(transfer_orbit.orbital_elements)
+
 # 3c - Determine dv required for earth departure
+print("\nPart 3c - dV for Earth departure:")
 r_at_earth_scalar = np.linalg.norm(r_at_earth)
 v1_transfer = np.linalg.norm(v_at_earth)
 transfer_h = np.dot(r_at_earth, v_at_earth)
-                         
+
 dv_earth_departure = interplanetary_transfer_dv(r_parked_earth,
                                                 r_at_earth_scalar,
                                                 mu_sun,
                                                 transfer_h,
                                                 v1_transfer,
-                                                mu_earth)
-print(f"Part 3c - Dv for Earth Departure: {dv_earth_departure}")
+                                                mu_earth,
+                                                print_v=False)
+print(f"dV for Earth Departure: {dv_earth_departure}")
 
 # 3d - determine Mars atmosphereic entry v and offset range
+print(f"\nPart 3d - Mars lander and orbiter")
+v2_transfer = np.linalg.norm(v_at_mars)
+v_inf_mars = util.get_v_inf(mu_sun, r_mars, r_mars, transfer_h, v2_transfer)
+dv_atmospheric_orbit = interplanetary_transfer_dv(r_final_lander,
+                                                  r_mars,
+                                                  mu_sun,
+                                                  transfer_h,
+                                                  v2_transfer,
+                                                  mu_mars,
+                                                  print_v=False)
+v_atmospheric_entry = dv_atmospheric_orbit + util.velo_from_radius(mu_mars,
+                                                                   r_final_lander,
+                                                                   r_final_lander)
+
+
+r_mars_surf = 3380*u.km
+offset_dist_surf = util.get_offset_dist(r_mars_surf, v_inf_mars, mu_mars)
+offset_dist_atmo = util.get_offset_dist(r_final_lander, v_inf_mars, mu_mars)
+offset_range = offset_dist_atmo - offset_dist_surf
+
+print(f"Velo at Mars Atmospheric Entry: {v_atmospheric_entry}")
+print(f"Offset range for Mars Lander: {offset_range}")
 
 # 3e - determine dv required for mars orbiter insertion
+print("\n3e - dV required for mars orbiter insertion")
+dv_orbiter = interplanetary_transfer_dv(r_final_orbiter,
+                                        r_mars,
+                                        mu_sun,
+                                        transfer_h,
+                                        v2_transfer,
+                                        mu_mars,
+                                        print_v=False)
+print(f"dV: {dv_orbiter}")

testTOF.py
@@ -1,8 +1,13 @@
 import numpy as np
 import astropy.units as u
 
-from orbits import Orbit, OrbitalElements
+from orbits import Orbit, OrbitalElements, interplanetary_transfer_dv
 import two_body_util as util
+
+TOF = False
+gauss = False
+transfer = False
+grav_assist = True
 
 def is_close(val_to_check, correct_val, margin=1e-3):
     vals = [val_to_check, correct_val]
@@ -17,151 +22,201 @@ def is_close(val_to_check, correct_val, margin=1e-3):
         close = False
     return close
 
-# TOF Kepler
-print("TOF Kepler")
-e = 0.85
-a = 15000*u.km
-theta0 = 15*u.deg
-tof = 8*u.hr
-meu = 1*util.MEU_EARTH
+if TOF:
+    # TOF Kepler
+    print("TOF Kepler")
+    e = 0.85
+    a = 15000*u.km
+    theta0 = 15*u.deg
+    tof = 8*u.hr
+    meu = 1*util.MEU_EARTH
 
-theta_final = util.predict_location(e, a, theta0, tof, 1, meu)
-print(theta_final)
+    theta_final = util.predict_location(e, a, theta0, tof, 1, meu)
+    print(theta_final)
 
-print(util.time_of_flight_kepler(e, a, theta0, theta_final, meu, 1).to(u.hr))
+    print(util.time_of_flight_kepler(e, a, theta0, theta_final, meu, 1).to(u.hr))
 
-print("TOF Kepler")
-e = 0.12
-a = 36225*u.km
-theta0 = 268*u.deg
-tof = 1.3*u.hr
-meu = 1*util.MEU_EARTH
+    print("TOF Kepler")
+    e = 0.12
+    a = 36225*u.km
+    theta0 = 268*u.deg
+    tof = 1.3*u.hr
+    meu = 1*util.MEU_EARTH
 
-theta_final = util.predict_location(e, a, theta0, tof, 0, meu)
-print(theta_final)
+    theta_final = util.predict_location(e, a, theta0, tof, 0, meu)
+    print(theta_final)
 
-print(util.time_of_flight_kepler(e, a, theta0, theta_final, meu, 0).to(u.hr))
+    print(util.time_of_flight_kepler(e, a, theta0, theta_final, meu, 0).to(u.hr))
 
-# TOF Ellipse
-print("TOF Ellipse")
-r = [2, 1, -1]*util.DU_EARTH
-v = [0.1, 0.5, 0.2]*util.DUTU_EARTH
-dt = 5*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+    # TOF Ellipse
+    print("TOF Ellipse")
+    r = [2, 1, -1]*util.DU_EARTH
+    v = [0.1, 0.5, 0.2]*util.DUTU_EARTH
+    dt = 5*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
-print(r_final, v_final)
+    r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
+    print(r_final, v_final)
 
-r = [1, -0.22, 0]*util.DU_EARTH
-v = [1, -0.1, 0.4]*util.DUTU_EARTH
-dt = 25*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+    r = [1, -0.22, 0]*util.DU_EARTH
+    v = [1, -0.1, 0.4]*util.DUTU_EARTH
+    dt = 25*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
-print(r_final, v_final)
+    r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
+    print(r_final, v_final)
 
-# TOF Hyperbolic
-print("TOF Hyperbolic")
-r = [-0.5, 1, 0.1]*util.DU_EARTH
-v = [1.2, 2, 0]*util.DUTU_EARTH
-dt = 100*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+    # TOF Hyperbolic
+    print("TOF Hyperbolic")
+    r = [-0.5, 1, 0.1]*util.DU_EARTH
+    v = [1.2, 2, 0]*util.DUTU_EARTH
+    dt = 100*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
-print(r_final, v_final)
+    r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
+    print(r_final, v_final)
 
-r = [1.1, -0.4, 0]*util.DU_EARTH
-v = [0.2, 0, 4.3]*util.DUTU_EARTH
-dt = 54*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+    r = [1.1, -0.4, 0]*util.DU_EARTH
+    v = [0.2, 0, 4.3]*util.DUTU_EARTH
+    dt = 54*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
-print(r_final, v_final)
+    r_final, v_final = util.time_of_flight_universal_var(r, v, dt, meu)
+    print(r_final, v_final)
 
-# Gauss Problem
-# From Lecture
-r1 = [1, 0, 0]*util.DU_EARTH
-r2 = [2, 3, -1]*util.DU_EARTH
-dt = 20*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+if gauss:
+    # Gauss Problem
+    # From Lecture
+    r1 = [1, 0, 0]*util.DU_EARTH
+    r2 = [2, 3, -1]*util.DU_EARTH
+    dt = 20*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-results = util.get_velo_gauss_problem(r1, r2, dt, meu, zguess=5 )
-print(results)
+    results = util.get_velo_gauss_problem(r1, r2, dt, meu, zguess=5 )
+    print(results)
 
-correct_ans = {"short": {"v1": [1.126, 0.555, -0.185],
-                        "v2": [-0.318, -0.199, 0.0664]},
-               "long": {"v1": [-0.899, -0.847, 0.282],
-                        "v2": [0.0477, -0.352, 0.177]}}
+    correct_ans = {"short": {"v1": [1.126, 0.555, -0.185],
+                            "v2": [-0.318, -0.199, 0.0664]},
+                "long": {"v1": [-0.899, -0.847, 0.282],
+                            "v2": [0.0477, -0.352, 0.177]}}
 
-passing = True
-for sol_type, vectors in results.items():
-    for vector in vectors:
-        for i in range(len(vector)):
-            close = is_close(results[sol_type][vector][i], 
-                             correct_ans[sol_type][vector][i])
-            if not close:
-                print(f"{results[sol_type][vector][i]} =/=" 
-                      f"{correct_ans[sol_type][vector][i]}")
-                passing = False
-                break
-if not passing:
-    print(f"Test failed")
+    passing = True
+    for sol_type, vectors in results.items():
+        for vector in vectors:
+            for i in range(len(vector)):
+                close = is_close(results[sol_type][vector][i], 
+                                correct_ans[sol_type][vector][i])
+                if not close:
+                    print(f"{results[sol_type][vector][i]} =/=" 
+                        f"{correct_ans[sol_type][vector][i]}")
+                    passing = False
+                    break
+    if not passing:
+        print(f"Test failed")
 
-# From Example Doc
+    # From Example Doc
 
-r1 = [.566, 1.1, -0.4]*util.DU_EARTH
-r2 = [0.33, 2.11, 0]*util.DU_EARTH
-dt = 8.55*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+    r1 = [.566, 1.1, -0.4]*util.DU_EARTH
+    r2 = [0.33, 2.11, 0]*util.DU_EARTH
+    dt = 8.55*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-results = util.get_velo_gauss_problem(r1, r2, dt, meu, zguess=5 )
-print(results)
+    results = util.get_velo_gauss_problem(r1, r2, dt, meu, zguess=5 )
+    print(results)
 
-correct_ans = {"short": {"v1": [0.263844, 0.886344, -0.12724],
-                        "v2": [-0.17499, -0.47812, 0.101745]},
-               "long": {"v1": [0.018046, -0.89061, -0.15975],
-                        "v2": [0.195142, -0.33997, -0.25212]}}
+    correct_ans = {"short": {"v1": [0.263844, 0.886344, -0.12724],
+                            "v2": [-0.17499, -0.47812, 0.101745]},
+                "long": {"v1": [0.018046, -0.89061, -0.15975],
+                            "v2": [0.195142, -0.33997, -0.25212]}}
 
-passing = True
-for sol_type, vectors in results.items():
-    for vector in vectors:
-        for i in range(len(vector)):
-            close = is_close(results[sol_type][vector][i], 
-                             correct_ans[sol_type][vector][i])
-            if not close:
-                print(f"{results[sol_type][vector][i]} =/=" 
-                      f"{correct_ans[sol_type][vector][i]}")
-                passing = False
-                break
-if not passing:
-    print(f"Test failed")
+    passing = True
+    for sol_type, vectors in results.items():
+        for vector in vectors:
+            for i in range(len(vector)):
+                close = is_close(results[sol_type][vector][i], 
+                                correct_ans[sol_type][vector][i])
+                if not close:
+                    print(f"{results[sol_type][vector][i]} =/=" 
+                        f"{correct_ans[sol_type][vector][i]}")
+                    passing = False
+                    break
+    if not passing:
+        print(f"Test failed")
 
-# From Example Doc
+    # From Example Doc
 
-r1 = [-1.1, 0, 0.55]*util.DU_EARTH
-r2 = [0, 0, -1]*util.DU_EARTH
-dt = 8.55*util.TU_EARTH
-meu = 1*util.MEU_EARTH
+    r1 = [-1.1, 0, 0.55]*util.DU_EARTH
+    r2 = [0, 0, -1]*util.DU_EARTH
+    dt = 8.55*util.TU_EARTH
+    meu = 1*util.MEU_EARTH
 
-results = util.get_velo_gauss_problem(r1, r2, dt, meu, zguess=5 )
-print(results)
+    results = util.get_velo_gauss_problem(r1, r2, dt, meu, zguess=5 )
+    print(results)
 
-correct_ans = {"short": {"v1": [-0.9095, 0, -0.301],
-                        "v2": [0.831332, 0, 0.774892]},
-               "long": {"v1": [0.16025, 0, 0.943095],
-                        "v2": [-1.12554, 0, 0.148431]}}
+    correct_ans = {"short": {"v1": [-0.9095, 0, -0.301],
+                            "v2": [0.831332, 0, 0.774892]},
+                "long": {"v1": [0.16025, 0, 0.943095],
+                            "v2": [-1.12554, 0, 0.148431]}}
 
-passing = True
-for sol_type, vectors in results.items():
-    for vector in vectors:
-        for i in range(len(vector)):
-            close = is_close(results[sol_type][vector][i], 
-                             correct_ans[sol_type][vector][i])
-            if not close:
-                print(f"{results[sol_type][vector][i]} =/=" 
-                      f"{correct_ans[sol_type][vector][i]}")
-                passing = False
-                break
-if not passing:
-    print(f"Test failed")
+    passing = True
+    for sol_type, vectors in results.items():
+        for vector in vectors:
+            for i in range(len(vector)):
+                close = is_close(results[sol_type][vector][i], 
+                                correct_ans[sol_type][vector][i])
+                if not close:
+                    print(f"{results[sol_type][vector][i]} =/=" 
+                        f"{correct_ans[sol_type][vector][i]}")
+                    passing = False
+                    break
+    if not passing:
+        print(f"Test failed")
 
+if transfer:
+    mu_sun = 1*util.MEU_SUN
+    r_earth = 1*util.AU_SUN
+    r_mars = 2.28e8*u.km
+    
+    # Phase2
+    transfer_energy = util.specific_energy_from_rpra(r_earth, r_mars, mu_sun)
+    print(f"Transfer Energy: {transfer_energy}")
+
+    v1 = util.velo_from_energy(transfer_energy, mu_sun, r_earth)
+    v2 = util.velo_from_energy(transfer_energy, mu_sun, r_mars)
+    print(f"v1 and v2:  {v1} {v2}")
+
+    # Phase1
+    mu_earth = 1*util.MEU_EARTH
+    h = util.angular_momentum_from_periapsis(v1, r_earth)
+    rp = 1*util.DU_EARTH + 300*u.km
+    dv1 = interplanetary_transfer_dv(rp,
+                                     r_earth,
+                                     mu_sun,
+                                     h,
+                                     v1,
+                                     mu_earth)
+    print(f"Delta V 1: {dv1}")
+
+    # Phase3
+    mu_mars = 4.28284e4*u.km**3/u.s**2
+    h = util.angular_momentum_from_periapsis(v1, r_mars)
+    rp2 = 3780*u.km
+    dv2 = interplanetary_transfer_dv(rp2,
+                                     r_mars,
+                                     mu_sun,
+                                     h,
+                                     v2,
+                                     mu_mars,
+                                     print_v=True)
+    print(f"Delta V2: {dv2}")
+
+    print(f"Total dV: {dv1 + dv2}")
+
+if grav_assist:
+    v_2 = np.array([[-0.50297], [0.518378], [0]])*util.AUTU_SUN
+    print(f"v_init mag: {np.linalg.norm(v_2)}")
+    v_mars = np.array([[-0.6116], [0.5317], [0]])*util.AUTU_SUN
+    rp = 3391*u.km
+    mu_mars = 4.28284e4*u.km**3/u.s**2
+
+    print(util.get_gravity_assist_velo(rp, v_2, v_mars, mu_mars, debug=False))

two_body_util.py
@@ -20,7 +20,7 @@ du_tu = (MEU_EARTH**(1/2)/DU_EARTH**(1/2), DUTU_EARTH,
 AU_SUN = u.def_unit("Au Sun", 1.496e8*u.km) 
 TU_SUN = u.def_unit("Tu Sun", 58.132821*u.d) 
 AUTU_SUN = u.def_unit("Au/Tu Sun", 29.784852*u.km/u.s) 
-MEU_SUN = u.def_unit("meu Sun", 1.3271544e11*u.km**3/u.s**2)
+MEU_SUN = u.def_unit("mu Sun", 1.3271544e11*u.km**3/u.s**2)
 
 spec_energy = u.km**2/u.s**2
 angular_momentum = u.km**2/u.s
@@ -54,7 +54,7 @@ def specific_energy_from_velo_infinity(velo):
 
 def specific_energy_from_rpra(rp, ra, meu):
     energy = -meu / (ra + rp)
-    return energy.to(SPEC_E_EARTH)
+    return energy.to(u.km**2/u.s**2)
 
 def angular_momentum_from_p(p, meu):
     momentum = np.sqrt(p*meu) 
@@ -80,10 +80,6 @@ def get_escape_velo(meu, radius):
 
 def get_plane_change_dv(v1, v2, di):
     di = ensure_rad(di)
-#    v1sqr = v1**2
-#    v2sqr = v2**2
-#    negterm = -2*v1*v2*cos(di.value)
-#    dv = np.sqrt(v1sqr + v2sqr + negterm)
     dv = np.sqrt(v1**2 + v2**2 - 2*v1*v2*cos(di.value))
     return dv
 
@@ -114,17 +110,70 @@ def get_sphere_of_influence(large_mass, small_mass, distance):
     r = distance * (small_mass / large_mass) ** (2/5)
     return r
 
-def get_flightpath_angle(angular_momentum, r, v):
-    cos_val = angular_momentum / (r * v)
+def get_flightpath_angle(angular_momentum, r, v, print_ang=False):
+    cos_val = (angular_momentum.to(u.km**2/u.s) / (r.to(u.km) * v.to(u.km/u.s)))
     if cos_val > 1 or cos_val < -1:
         angle = 0*u.rad
     else:
-        angle = np.arccos(angular_momentum / (r * v))
+        angle = np.arccos((angular_momentum.to(u.km**2/u.s) / (r * v)))
+    if print_ang:
+        print(f"h: {angular_momentum}")
+        print(f"r,v: {r} {v}")
+        print(f"cos_val: {cos_val}")
+        print(f"Flightpath angle raw: {angle.to(u.deg)}")
+    if angle > (np.pi / 2)*u.rad:
+        angle = angle - (np.pi / 2)*u.rad
+    elif angle < (-np.pi / 2)*u.rad:
+        angle = angle + (np.pi / 2)*u.rad
+    if print_ang:
+        print(f"Flightpath angle final: {angle.to(u.deg)}")
     return angle
 
-def get_offset_range(offset_dist, r_target, r_surface):
-    offset_range = offset_dist* (r_target - r_surface)
-    return offset_range
+def get_v_inf(mu, r_planet, a_planet, transfer_angular_momentum, v_transfer,
+              prints=False):
+    v_planet = velo_from_radius(mu, r_planet, a_planet)
+    flightpath_angle = get_flightpath_angle(transfer_angular_momentum, 
+                                            r_planet, 
+                                            v_transfer,
+                                            print_ang=prints)
+    v_inf = (v_transfer * np.cos(flightpath_angle)) - v_planet
+    if prints:
+        print(f"v_planet: {v_planet}")
+        print(f"v_inf: {v_inf}")
+    return v_inf
+
+def get_best_turn_angle(rp, v_inf, mu, debug=False):
+    if debug:
+        print(rp)
+        print(v_inf.to(u.km/u.s))
+        print(mu)
+    e = (1 + ((rp * v_inf**2) / mu))
+    delta = 2*np.arcsin(1 / e)
+    if debug:
+        print(f"Max Delta: {delta.to(u.deg)}")
+        print(f"e: {e}")
+    return delta, e
+
+def get_gravity_assist_velo(rp, v_transfer, v_planet, mu, debug=False):
+    v_inf = v_transfer - v_planet
+    v_inf_magnitude = np.linalg.norm(v_inf)
+    d, e = get_best_turn_angle(rp, v_inf_magnitude, mu, debug=debug)
+    R = np.array([[np.cos(d), -np.sin(d), 0],
+                  [np.sin(d), np.cos(d), 0],
+                  [0, 0, 1]])
+    v_inf2 = np.dot(R, v_inf)
+    v2_mag = np.linalg.norm(v_inf2)
+    if debug:
+        print(f"v2_mag: {v2_mag}")
+        print(f"v2: {v_inf2}")
+    v_final = v_inf2 + v_planet
+    v_final_mag = np.linalg.norm(v_final)
+    return v_final, v_final_mag
+
+def get_offset_dist(r_target, v_inf, mu_body):
+    offset_dist = (r_target / v_inf) * \
+                    np.sqrt(v_inf ** 2 + (2 * mu_body / r_target))
+    return offset_dist
 
 #************************ Time of Flight *************************
 def time_of_flight_kepler(e, a, theta1, theta2, meu, pass_periapsis=0):